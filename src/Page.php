4b614e89840536eda664483896393379aae07411
Jean-Pierre Gassin <jeanpierre.gassin@gmail.com>
2018-03-13 10:44:45 +1000

    Allow page blocks to be searched

--- src/Page.php
+++ src/Page.php
@@ -19,8 +19,17 @@ class Page extends \TCG\Voyager\Models\Page
     {
         $array = $this->toArray();
 
-        // Customize array...
+        if (!class_exists('\Pvtl\VoyagerPageBlocks\Page')) {
+            return $array;
+        }
 
-        return $array;
+        // Include page block data to be "Searchable"
+        $pageBlockPageModel = new \Pvtl\VoyagerPageBlocks\Page();
+        $pageBlocks = $pageBlockPageModel->blocks()->get(['data'])->map(function ($block) {
+            return json_encode($block['data']);
+        });
+
+        $array['page_blocks'] = implode(' ', $pageBlocks->toArray());
+
     }
 }
