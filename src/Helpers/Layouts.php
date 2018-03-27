f1cc0d8fb30c700a4748fd9118d2f706f97536e9
Matt Mcnamee <matt@mcnamee.co>
2018-03-27 12:18:24 +1000

    All the use of other view directories

--- src/Helpers/Layouts.php
+++ src/Helpers/Layouts.php
@@ -9,18 +9,18 @@ class Layouts
      * @param string $module
      * @return array
      */
-    public static function getLayouts($module)
+    public static function getLayouts($module, $directory = 'layouts')
     {
         $layouts = array();
 
         // Get list of layouts from module
-        $vendorLayoutsDir = base_path("vendor/pvtl/$module/resources/views/layouts");
+        $vendorLayoutsDir = base_path("vendor/pvtl/$module/resources/views/$directory");
         if (is_dir($vendorLayoutsDir)) {
             $layouts = scandir($vendorLayoutsDir);
         }
 
         // Get list of layouts from project
-        $projectLayoutsDir = resource_path("views/vendor/$module/layouts");
+        $projectLayoutsDir = resource_path("views/vendor/$module/$directory");
         if (is_dir($projectLayoutsDir)) {
             $layouts = array_merge($layouts, scandir($projectLayoutsDir));
         }
