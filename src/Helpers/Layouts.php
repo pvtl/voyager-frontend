26a77c3efde8b23e0413adf1b03b9e7154e78a2d
Jean-Pierre Gassin <jeanpierre.gassin@gmail.com>
2018-03-27 11:22:32 +1000

    Layout fetcher is now a static function

---
+++ src/Helpers/Layouts.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Pvtl\VoyagerFrontend\Helpers;
+
+class Layouts
+{
+    /**
+     * Scans project directories to retrieve a list of layouts
+     * @param string $module
+     * @return array
+     */
+    public static function getLayouts($module)
+    {
+        $layouts = array();
+
+        // Get list of layouts from module
+        $vendorLayoutsDir = base_path("vendor/pvtl/$module/resources/views/layouts");
+        if (is_dir($vendorLayoutsDir)) {
+            $layouts = scandir($vendorLayoutsDir);
+        }
+
+        // Get list of layouts from project
+        $projectLayoutsDir = resource_path("views/vendor/$module/layouts");
+        if (is_dir($projectLayoutsDir)) {
+            $layouts = array_merge($layouts, scandir($projectLayoutsDir));
+        }
+
+        foreach ($layouts as $i => $layout) {
+            // Only include files that are .blade.php files
+            if (strpos($layout, '.blade.php') === false) {
+                unset($layouts[$i]);
+                continue;
+            }
+
+            // Strip out .blade.php for DB reference
+            $layouts[$i] = str_replace('.blade.php', '', $layout);
+        }
+
+        // Remove duplicates
+        $layouts = array_unique($layouts);
+
+        // Reset indexes
+        $layouts = array_values($layouts);
+
+        // Reset indexes and return
+        return $layouts;
+    }
+}
