df900c9ec3070323f11592ad64297aaf2add04c2
Jean-Pierre Gassin <jeanpierre.gassin@gmail.com>
2018-04-19 11:21:34 +1000

    PageController only adds breadcrumbs now

--- src/Http/Controllers/PageController.php
+++ src/Http/Controllers/PageController.php
@@ -2,26 +2,9 @@
 
 namespace Pvtl\VoyagerFrontend\Http\Controllers;
 
-use Pvtl\VoyagerFrontend\Page;
 use Pvtl\VoyagerFrontend\Traits\Breadcrumbs;
 
 class PageController extends \Pvtl\VoyagerPages\Http\Controllers\PageController
 {
     use Breadcrumbs;
-
-    /**
-     * Route: Gets a single page and passes data to a view
-     *
-     * @param string $slug
-     *
-     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
-     */
-    public function getPage($slug = 'home')
-    {
-        $page = Page::where('slug', '=', $slug)->firstOrFail();
-
-        return view('voyager-frontend::modules/pages/default', [
-            'page' => $page,
-        ]);
-    }
 }
