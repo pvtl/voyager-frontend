0597dacd6821a803592d9667d01c4951b7a46653
Matt McNamee <matt@mcnamee.co>
2019-05-17 09:11:07 +1000

    Don’t precompile assets in install comment
    It’s in the readme instal instructions and currently causing an install error

--- src/Commands/InstallCommand.php
+++ src/Commands/InstallCommand.php
@@ -89,8 +89,7 @@ class InstallCommand extends Command
         $this->info('Updating Root package.json to include dependencies');
 
         $process = new Process('
-            npm install jquery what-input foundation-sites scrollreveal motion-ui --save-dev &&
-            npm run dev
+            npm install jquery what-input foundation-sites scrollreveal motion-ui --save-dev
         ');
         $process->setTimeout(null); // Setting timeout to null to prevent installation from stopping at a certain point in time
         $process->setWorkingDirectory(base_path())->mustRun();
